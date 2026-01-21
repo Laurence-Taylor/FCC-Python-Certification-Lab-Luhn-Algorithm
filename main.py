26eea8e7200b300d150da229e435287680e6a1f4
LaurenceTaylor <morfeus06388casa@gmail.com>
2026-01-21 13:51:34 -0600

    Fix: Fixing all bugs

--- main.py
+++ main.py
@@ -5,15 +5,24 @@ def verify_card_number(card):
     card_number_inverted = card_number[::-1]
     total_sum = 0
     # For each element in the inverted card number
-    for i in range(1,len(card_number_inverted)):
-        if (i%2) == 0:
-            duplicate_digit = int(card_number_inverted[i]) * 2
-            if duplicate_digit > 9:
-                total_sum += (duplicate_digit - 9)
+    for i in range(len(card_number_inverted)):
+        # Check if is an even digit
+        if ((i+1)%2) == 0:
+            # Duplicate de digit
+            duplicated_digit = int(card_number_inverted[i]) * 2
+            # If result has two digits
+            if duplicated_digit > 9:
+                # Delete 9 to the duplicated_digit and sum to the total
+                total_sum += (duplicated_digit - 9)
             else:
-                total_sum += duplicate_digit
+                # Sum duplicated_digit to the total
+                total_sum += duplicated_digit
         else:
+            # Sum the digit to the total
             total_sum +=  int(card_number_inverted[i])
-    return total_sum
+    # Return vaid total
+    if total_sum % 10 == 0:
+        return 'VALID!'
+    return 'INVALID!'
 if __name__ == '__main__':
     print(verify_card_number('4111-1111-1111-1111'))
